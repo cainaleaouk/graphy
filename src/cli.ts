d3f028d9782332527f8eac24f5281c9970b41c5c
Cai Leao <cainaleao.uk@gmail.com>
2019-08-04 22:19:39 +0100

    add CLI runner for graph output

---
+++ src/cli.ts
@@ -0,0 +1,30 @@
+import 'isomorphic-fetch';
+
+import commander from 'commander';
+
+commander
+  .option('--symbol <symbol>', 'company stocks symbol')
+  .option('--since <since>', 'starting date')
+  .option('--until <until>', 'ending date')
+  .option('--price <price>', 'price type (`open`, `high`, `low` or `close`)', 'close');
+
+commander.parse(process.argv);
+
+const { symbol, since, until, price } = commander;
+
+if (!symbol) throw 'symbol parameter is missing!';
+if (!since) throw 'since parameter is missing!';
+if (!until) throw 'until parameter is missing!';
+
+const URL = `http://localhost:8000/ascii?symbol=${symbol}&since=${since}&until=${until}&price=${price}`;
+
+fetch(URL)
+  .then((response) => {
+    return response.text();
+  })
+  .then((graphData) => {
+    console.log(graphData);
+  })
+  .catch((e) => {
+    console.error(e);
+  });
